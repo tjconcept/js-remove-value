b2ad2018f52b0aa3c412d899389680830ee7648b
Thomas Jensen <tj@inze.it>
2014-04-08 17:24:42 +0200

    Add another test

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ var test = require('tap').test;
 test('remove-value', function( t ) {
 	t.deepEqual(removeValue([ 'apple', 'lemon', 'banana', 'lemon' ], 'lemon'), [ 'apple', 'banana' ] );
 	t.deepEqual(removeValue([ 'apple', 'lemon', 'banana', 'lemon' ], 'lemon', 1), [ 'apple', 'banana', 'lemon' ] );
+	t.deepEqual(removeValue([ 'apple', 'lemon' ], 'not there', 1), [ 'apple', 'lemon' ] );
 
 	Array.prototype.remove = removeValue;
 
